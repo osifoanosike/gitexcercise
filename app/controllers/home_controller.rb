ef4e4a273c09b1479695cc40f195f75fe6e67517
Anosike Osifo <osifo@dealdey.com>
2014-11-29 10:48:43 +0100

    Added home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def aboutUs
+  end
+
+  def profolio
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/aboutUs'
+
+  get 'home/profolio'
+
+  get 'home/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
